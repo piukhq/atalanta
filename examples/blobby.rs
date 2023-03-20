312cf05a306555ae1f1bd2cfce25bbadac04ea6d
backwardspy <backwardspy@pigeon.life>
2023-03-20 14:59:05 +0000

    fix: async function call in blobby example

--- examples/blobby.rs
+++ examples/blobby.rs
@@ -1,34 +1,31 @@
 use azure_storage::prelude::*;
 use azure_storage_blobs::prelude::*;
 use color_eyre::Result;
-//use tokio::runtime::Handle;
 
-async fn send_to_blob_storage(data: &str) -> Result<()>{
-    //let handle = Handle::current();
-    //handle.enter();
-    
+#[tokio::main]
+async fn send_to_blob_storage(data: String) -> Result<()> {
     let account: String = "binkuksouthdev".to_string();
-    let access_key = "L/xU6NZswZAJbFhKjIGr0feakhY8QsCw4oUuj6bXNfxhWQv2caNkDo8czIu05DBcaZbSL7vfpYGP7OZsbpXuhw==".to_string();
+    let access_key =
+        "L/xU6NZswZAJbFhKjIGr0feakhY8QsCw4oUuj6bXNfxhWQv2caNkDo8czIu05DBcaZbSL7vfpYGP7OZsbpXuhw=="
+            .to_string();
     let container: String = "harmonia-imports-test".to_string();
-    let blob_name:String = "wasabi".to_string();
-    
+    let blob_name: String = "wasabi".to_string();
+
     let storage_credentials = StorageCredentials::Key(account.clone(), access_key);
-    let blob_client = ClientBuilder::new(account, storage_credentials).blob_client(&container, blob_name);
+    let blob_client =
+        ClientBuilder::new(account, storage_credentials).blob_client(&container, blob_name);
 
-    blob_client.put_block_blob("testing blobs").content_type("text/plain").await?;
     println!("upload blob: {data}");
-    Ok(())
-}
 
-fn send_blob(data: String) -> Result<()> {
-    let result = futures::executor::block_on(send_to_blob_storage(&data));
-
-    println!("{:?}", result);
+    blob_client
+        .put_block_blob(data)
+        .content_type("text/plain")
+        .await?;
     Ok(())
 }
 
 fn main() -> Result<()> {
-    send_blob("Some test data for harmonia perf testing".to_string())?;
+    send_to_blob_storage("Some test data for harmonia perf testing".to_owned())?;
 
     Ok(())
 }
