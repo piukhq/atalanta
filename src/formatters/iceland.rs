5bd7ae960459a49410d965767b871c3efab2af13
mlatham <ml@bink.com>
2023-07-24 16:04:06 +0100

    Iceland date correction and file updates

--- src/formatters/iceland.rs
+++ src/formatters/iceland.rs
@@ -1,5 +1,8 @@
 #![warn(clippy::unwrap_used, clippy::expect_used)]
 use crate::{formatters::to_pounds, models::Transaction};
+use chrono_tz::Europe::London;
+use chrono::{Duration, TimeZone};
+use chrono::prelude::*;
 use color_eyre::Result;
 use csv::WriterBuilder;
 use serde::Serialize;
@@ -51,10 +54,7 @@ impl Formatter for IcelandFormatter {
                 card_scheme_id: "6".to_owned(),
                 card_scheme_name: card_type_name(transaction.payment_provider.as_str()),
                 identifier: transaction.identifier.clone(),
-                transaction_date: transaction
-                    .transaction_date
-                    .format("%Y-%m-%d %H:%M:%S")
-                    .to_string(),
+                transaction_date: date_to_timezone(&transaction.transaction_date),
                 amount: to_pounds(transaction.amount),
                 amount_unit: "GBP".to_owned(),
                 cashback_value: ".00".to_owned(),
@@ -78,6 +78,11 @@ fn card_type_name(payment_provider: &str) -> String {
         "visa" => "Visa".to_owned(),
         _ => "Unknown".to_owned(),
     }
+}
+
+fn date_to_timezone(date: &DateTime<Utc>) -> String {
+    let tz_date = date.with_timezone(&London);
+    return tz_date.format("%Y-%m-%d %H:%M:%S").to_string();
 }
 
 #[cfg(test)]
@@ -125,4 +130,21 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn convert_to_timezone_datetime() -> Result<()> {
+        // All transaction dates and times are generated as UTC. Therefore we
+        // need to make datetimes created during summer months for Iceland to be aware of daylight savings.
+        // Check the time changes across a daylight savings change
+        let naivedatetime_utc = NaiveDate::from_ymd_opt(2016, 10, 29).unwrap().and_hms_opt(12, 0, 0).unwrap();
+        let datetime_utc = DateTime::<Utc>::from_utc(naivedatetime_utc, Utc);
+
+        let day_later = datetime_utc + Duration::hours(24); // same datetime 24 hours later in GMT/UTC
+
+
+        assert_eq!("2016-10-29 13:00:00", date_to_timezone(&datetime_utc));
+        assert_eq!("2016-10-30 12:00:00", date_to_timezone(&day_later));
+        Ok(())
+    }
+
 }
